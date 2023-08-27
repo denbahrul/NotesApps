ef3cb7aa7826bf3d471261af47979425e6889562
denbahrul <ulum0326@gmail.com>
2023-08-27 23:24:25 +0700

    membuat UI aplikasi

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ function Header () {
     return (
         <div className="note-app__header">
             <h1>My Notes</h1>
+            <input type="text" placeholder="Cari catatan ..."/>
         </div>
     )
 }
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -9,8 +9,10 @@ function MainPage() {
             <Header/>
             <div className="note-app__body">
                 <FormInput/>
+                <h2>Catatan Aktif</h2>
                 <NoteList/>
-                <p>Arsip Catatan</p>
+                <h2>Arsip</h2>
+                <NoteList/>
             </div>
         </div>
     )
