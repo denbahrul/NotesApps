d22b1f6095269e4a1c8308fec2c8708453dcaffc
denbahrul <ulum0326@gmail.com>
2023-08-28 14:24:58 +0700

    Menambahkan controlled component (form)

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,17 +1,59 @@
 import React from "react";
 
-function FormInput() {
-    return (
-        <div className="note-input">
-            <h2 className="note-input__title">Buat catatan</h2>
-            <form className="note-input__body">
-                <p className="note-input__title__char-limit">Sisa karakter: 50</p>
-                <input type="text" placeholder="Judul catatan..."/>
-                <textarea type="text" placeholder="Isi catatan..."/>
-                <button type="submit">Buat</button>
-            </form>
-        </div>
-    )
+class FormInput extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            title:'',
+            body:'',
+            limitChar: 50,
+        };
+
+        this.onTitleChangeHandler = this.onTitleChangeHandler.bind(this);
+        this.onBodyChangeHandler = this.onBodyChangeHandler.bind(this);
+        this.onAddNoteEventHandler = this.onAddNoteEventHandler.bind(this);
+    }
+
+    onTitleChangeHandler(event) {
+        const maxChar = 50;
+        const titleInput = event.target.value.slice(0, maxChar);
+        const titleInputLength = titleInput.length;
+
+        this.setState(() => {
+            return {
+                title: titleInput,
+                limitChar: maxChar - titleInputLength,
+            };
+        });
+    }
+
+    onBodyChangeHandler(event) {
+        this.setState(() => {
+            return {
+                body: event.target.value
+            };
+        });
+    }
+
+    onAddNoteEventHandler(event) {
+        event.preventDefault();
+        this.props.addNotes(this.state);
+    }
+
+    render() {
+        return (
+            <div className="note-input">
+                <h2 className="note-input__title">Buat catatan</h2>
+                <form className="note-input__body" onSubmit={this.onAddNoteEventHandler}>
+                    <p className="note-input__title__char-limit">Sisa karakter: {this.state.limitChar}</p>
+                    <input type="text" placeholder="Judul catatan..." value={this.state.title} onChange={this.onTitleChangeHandler}/>
+                    <textarea type="text" placeholder="Isi catatan..." value={this.state.body} onChange={this.onBodyChangeHandler}/>
+                    <button type="submit">Buat</button>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default FormInput;
