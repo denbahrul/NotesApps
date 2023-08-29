d812595352906a4aa4b37cf254a728f88a4aee0b
denbahrul <ulum0326@gmail.com>
2023-08-29 13:51:13 +0700

    menambahkan styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header ({onSearch, search}) {
     return (
         <div className="note-app__header">
             <h1>My Notes</h1>
-            <input type="text" placeholder="Cari catatan ..." value={search} onChange={onSearch}/>
+            <input type="text" placeholder="Cari catatan ..." className="note-app_search" value={search} onChange={onSearch}/>
         </div>
     )
 }
