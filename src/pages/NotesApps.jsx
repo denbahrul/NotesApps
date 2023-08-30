8229c58038fb70b775ef10dfc71b9c7e82a87efe
denbahrul <ulum0326@gmail.com>
2023-08-30 18:29:44 +0700

    menambahkan fitur login dan register

--- src/pages/NotesApps.jsx
+++ src/pages/NotesApps.jsx
@@ -1,13 +1,65 @@
 import React from "react";
-import MainPage from "./MainPage";
 import { Routes, Route } from "react-router-dom";
 import DetailPage from "./DetailPage";
 import ArchivePage from "./ArchivePage";
 import AddNotePage from "./AddNotePage";
 import HomePage from "./HomePage";
 import Header from "../components/Header";
+import { useEffect } from "react";
+import LoginPage from "./LoginPage";
+import RegisterPage from "./RegisterPage";
+import { getUserLogged, putAccessToken } from "../utils/network-data";
 
 function NotesApps() {
+    const [authedUser, setAuthedUser] = React.useState(null);
+    const [theme, setTheme] = React.useState(localStorage.getItem("theme") || "dark");
+    const [initializing, setInitializing] = React.useState(true);
+
+    React.useEffect(() => {
+        async function fetchUserData() {
+          const { data } = await getUserLogged();
+          setAuthedUser(data);
+          setInitializing(false);
+        }
+    
+        fetchUserData();
+    
+        return () => {
+          setAuthedUser(null);
+        };
+      }, []);
+
+    async function onLoginSucces({ accessToken }) {
+        putAccessToken(accessToken);
+    
+        const { data } = await getUserLogged();
+        setAuthedUser(data);
+      }
+
+      if (initializing) {
+        return (
+          <div className="app-container">
+            <p>
+              <strong>Loading Page</strong>
+            </p>
+          </div>
+        );
+      }
+
+    if ( authedUser === null ) {
+        return(
+            <div>
+                <Header/>
+                <div  className="note-app__body">
+                    <Routes>
+                        <Route path="/*" element={<LoginPage loginSuccess={onLoginSucces}/>}/>
+                        <Route path="/register" element={<RegisterPage/>}/>
+                    </Routes>
+                </div>
+            </div>
+        );
+    }
+
     return(
         <div>
             <Header/>
@@ -20,7 +72,7 @@ function NotesApps() {
                 </Routes>
             </div>
         </div>
-    )
+    );
 }
 
 export default NotesApps;
